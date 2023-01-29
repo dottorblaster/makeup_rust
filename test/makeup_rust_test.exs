38304037f70ad7d0675e5d9fc7794baf5791a443
Alessio Biancalana <dottorblaster@gmail.com>
2023-01-29 20:12:40 +0100

    Add a failing test, waiting for a proper testing strategy to come up

--- test/makeup_rust_test.exs
+++ test/makeup_rust_test.exs
@@ -2,6 +2,19 @@ defmodule MakeupRustTest do
   use ExUnit.Case
 
   test "greets the world" do
-    assert MakeupRust.hello() == :world
+    assert Makeup.Lexers.RustLexer.lex("use crate::{Type, subcrate};") == [
+             {:keyword, %{language: :rust}, "use"},
+             {:whitespace, %{language: :rust}, " "},
+             {:keyword, %{language: :rust}, "crate"},
+             {:operator, %{language: :rust}, ":"},
+             {:operator, %{language: :rust}, ":"},
+             {:punctuation, %{group_id: "3158012706-1", language: :rust}, "{"},
+             {:name_constant, %{language: :rust}, ["T", "ype"]},
+             {:punctuation, %{language: :rust}, ","},
+             {:whitespace, %{language: :rust}, " "},
+             {:name, %{language: :rust}, "subcrate"},
+             {:punctuation, %{group_id: "3158012706-1", language: :rust}, "}"},
+             {:punctuation, %{language: :rust}, ";"}
+           ]
   end
 end
